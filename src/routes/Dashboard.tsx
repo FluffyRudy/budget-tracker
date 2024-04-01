3e9b753d0934ded9bc43245344c6f6d8b13cd8d4
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-04-01 10:30:50 +0545

    remove userStore

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { UserState } from "../states/User";
 import HeaderAndSummery from "../components/HeaderAndSummery";
 import BudgetDisplay from "../components/BudgetsDisplay";
 
 export default function Dashboard() {
-  const userState = UserState();
   return (
     <div className=''>
       <HeaderAndSummery />
